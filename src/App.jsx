7e7bf56bcfc16633e0adcade1ec24899205a5c16
darpananeja <darpan.aneja@grazitti.com>
2023-05-30 20:24:05 +0530

    New app new git

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import { Header } from './components';
+import { Todos } from './containers';
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Todos />
+    </>
+  )
+}
+
+export default App
---
+++ src/containers/Todos.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Todo } from '../components';
+
+const Todos = () => {
+  return (
+    <Todo />
+  )
+}
+
+export default Todos;
