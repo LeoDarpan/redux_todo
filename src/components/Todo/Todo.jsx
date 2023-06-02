c4c67457430827caf267518e65c83c262e77fa1d
Darpan Aneja <er.darpananeja@gmail.com>
2023-06-02 16:57:41 +0530

    Todo Actions UI

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -3,7 +3,15 @@ import { IconButton } from '../subcomponents';
 
 const Todo = () => {
   const changeState = (e) => {
-    console.log(e.target);
+    const buttons = document.querySelectorAll('.icon-button');
+    const indicator = document.querySelector('.active-indicator');
+    for(let [index, button] of buttons.entries()) {
+      button.classList.remove('active');
+      if(button.classList == e.target.classList) {
+        button.classList.add('active');
+        indicator.style.left = 10 + (24*index) + (10*index) + 'px';
+      }
+    }
   };
 
   return (
@@ -12,7 +20,8 @@ const Todo = () => {
         This is a sample text.
       </div>
       <div className="todo__actions">
-        <IconButton iconCode='&#xe876;' className='finished' handler={changeState}/>
+        <span className='active-indicator'></span>
+        <IconButton iconCode='&#xe876;' className='finished active' handler={changeState}/>
         <IconButton iconCode='&#xf726;' className='in-progress' handler={changeState}/>
         <IconButton iconCode='&#xef64;' className='pending' handler={changeState}/>
       </div>
